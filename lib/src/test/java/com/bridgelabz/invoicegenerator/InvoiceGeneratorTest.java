341d68d2de7b39a03ee997fd11a29b36ea807ea9
Akshaya HP <akshayhulkuli@gmail.com>
2021-09-22 18:13:43 +0530

    ADDED: test method to check if correct invoice summary is generated

--- lib/src/test/java/com/bridgelabz/invoicegenerator/InvoiceGeneratorTest.java
+++ lib/src/test/java/com/bridgelabz/invoicegenerator/InvoiceGeneratorTest.java
@@ -2,11 +2,16 @@ package com.bridgelabz.invoicegenerator;
 
 import org.junit.Test;
 import org.junit.Assert;
+import org.junit.Before;
 
 public class InvoiceGeneratorTest {
+	InvoiceGenerator invoiceGenerator;
+	@Before
+	public void init() {
+		invoiceGenerator = new InvoiceGenerator();
+	}
 	@Test
 	public void givenDistanceAndTime_ShouldReturnTotalfare() {
-		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
 		double distance =2.0;
 		int time = 5;
 		double fare  = invoiceGenerator.calculateFare(distance, time);
@@ -14,7 +19,6 @@ public class InvoiceGeneratorTest {
 	}
 	@Test
 	public void givenLessDistanceAndTime_ShouldReturnMinimumfare() {
-		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
 		double distance =0.1;
 		int time = 1;
 		double fare  = invoiceGenerator.calculateFare(distance, time);
@@ -22,11 +26,20 @@ public class InvoiceGeneratorTest {
 	}
 	@Test
 	public void givenMultipleRides_ShouldReturnTotalfare() {
-		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
 		Ride[] rides = { new Ride(10, 3),
 						 new Ride(15, 2)
 					   	};
 		double fare  = invoiceGenerator.calculateFare(rides);
 		Assert.assertEquals(255,fare,0.0);
+	}
+	
+	@Test
+	public void givenMultipleRides_ShouldReturnInvoiceSummary() {
+		Ride[] rides = { new Ride(10, 3),
+						 new Ride(15, 2)
+					   	};
+		InvoiceSummary invoiceSummary  = invoiceGenerator.getSummary(rides);
+		InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2,255,127.5);
+		Assert.assertEquals(expectedInvoiceSummary, invoiceSummary);
 	}
 }
